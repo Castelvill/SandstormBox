7b022f13e1047ec8525a40132023ef9c8a7d95af
Castelvill <789mati@wp.pl>
2023-11-13 16:00:06 +0100

    Destroy engine

--- src/main.cpp
+++ src/main.cpp
@@ -75,8 +75,9 @@ int main(){
     loadFontsToContainer(FontContainer, Threads.back().EXE_PATH);
 
     Layers.push_back(LayerClass("KERNEL", Threads.back().layersIDs, true, vec2d(0.0, 0.0), Threads.back().getScreenSize()));
+    Layers.back().Objects.push_back(AncestorObject("init", Layers.back().objectsIDs, Layers.back().getID()));
+    Layers.back().Objects.back().bindedScripts.push_back(Threads.back().EXE_PATH + "init/init.txt");
     Layers.back().Objects.push_back(AncestorObject("KERNEL", Layers.back().objectsIDs, Layers.back().getID()));
-    Layers.back().Objects.back().bindedScripts.push_back(Threads.back().EXE_PATH + "scripts/KERNEL.txt");
     Layers.back().Objects.back().translateAllScripts(true);
     
     if(Threads.back().isLayersUniquenessViolated(Layers)){
